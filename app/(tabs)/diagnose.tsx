1e4fe5f6b89bf12ef30eac09a8ee38346c201244
babusra <busrarigun@gmail.com>
2025-08-31 19:35:25 +0300

    tabBar and tab screens created

---
+++ app/(tabs)/diagnose.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const DiagnoseScreen = () => {
+  return (
+    <View>
+      <Text>DiagnoseScreen</Text>
+    </View>
+  )
+}
+
+export default DiagnoseScreen
---
+++ app/(tabs)/garden.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const GardenScreen = () => {
+  return (
+    <View>
+      <Text>GardenScreen</Text>
+    </View>
+  )
+}
+
+export default GardenScreen
