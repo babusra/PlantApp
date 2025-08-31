3b92e40cba9bf2d02f8261c989e4f8dd4edf9b2e
babusra <busrarigun@gmail.com>
2025-08-31 19:34:19 +0300

    app slice integrated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { useColorScheme } from "@/src/hooks/useColorScheme";
+import { store } from "@/src/redux/store";
+import { useAppSelector } from "@/src/redux/store/hooks";
 import {
   Rubik_300Light,
   Rubik_400Regular,
@@ -8,19 +10,33 @@ import {
   Rubik_800ExtraBold,
   useFonts,
 } from "@expo-google-fonts/rubik";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
+import React from "react";
 import "react-native-reanimated";
+import { Provider } from "react-redux";
+
+function InnerNavigator() {
+  const colorScheme = useColorScheme();
+  const isStarted = useAppSelector((s) => s.app.isStarted);
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Protected guard={!isStarted}>
+          <Stack.Screen name="(onboarding)" />
+        </Stack.Protected>
+        <Stack.Protected guard={isStarted}>
+          <Stack.Screen name="(tabs)" />
+        </Stack.Protected>
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [isStarted, setIsStarted] = useState(false);
   const [fontsLoaded] = useFonts({
     Rubik_300Light,
     Rubik_400Regular,
@@ -30,23 +46,11 @@ export default function RootLayout() {
     Rubik_800ExtraBold,
   });
 
-  if (!fontsLoaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  if (!fontsLoaded) return null;
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Protected guard={!isStarted}>
-          <Stack.Screen name="(onboarding)" />
-        </Stack.Protected>
-
-        <Stack.Protected guard={isStarted}>
-          <Stack.Screen name="(tabs)" />
-        </Stack.Protected>
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Provider store={store}>
+      <InnerNavigator />
+    </Provider>
   );
 }
